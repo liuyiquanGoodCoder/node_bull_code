fbc8f5f55bd8eb38e3b05699f9931263cabbb572
none Frontier <liuyiquancov5@gmail.com>
2019-04-13 11:38:43 +0800

    add 2.js

---
+++ 2.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+	input:process.stdin,
+	output:process.stdout
+}); 
+
+
+let count = 0,
+	n,
+	m,
+	c,
+	err = 0;
+
+
+let Beads = [5,6,7];
