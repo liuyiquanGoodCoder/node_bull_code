4f565d35ca7d05a4c6b2ff3e3b0a123db46023f0
none Frontier <liuyiquancov5@gmail.com>
2019-04-13 11:33:00 +0800

    fix a error

--- 1.js
+++ 1.js
@@ -1,3 +1,10 @@
+// 输入： 第1行为n代表用户的个数 
+// 第2行为n个整数，第i个代表用户标号
+// 为i的用户对某类文章的喜好度 
+// 第3行为一个正整数q代表查询的组数  
+// 第4行到第（3+q）行，每行包含3个整数l,r,k代表一组查询，
+// 即标号为l<=i<=r的用户中对这类文章喜好值为k的用户的个数。 
+// 数据范围n <= 300000,q<=300000 k是整型
 const readline = require('readline');
 const rl = readline.createInterface({
     input:process.stdin,
@@ -8,7 +15,7 @@ let usercount = 0;
 let groupcount = 0;
 let likeMap = {};
 
-rl.on('line',function(line) {
+rl.on("line",function(line) {
     if (linecount==0) {
         usercount = parseInt(line);
         linecount++;
@@ -35,7 +42,7 @@ rl.on('line',function(line) {
 
         if (likeMap[key]) {
             likeMap[key].forEach(function(tag) {
-                if (tag<=r && tag>=1) {
+                if (tag<=r && tag>=l) {
                     result++;
                 }
             })
@@ -51,11 +58,3 @@ rl.on('line',function(line) {
 
     }
 })
-
-// 输入： 第1行为n代表用户的个数 
-// 第2行为n个整数，第i个代表用户标号
-// 为i的用户对某类文章的喜好度 
-// 第3行为一个正整数q代表查询的组数  
-// 第4行到第（3+q）行，每行包含3个整数l,r,k代表一组查询，
-// 即标号为l<=i<=r的用户中对这类文章喜好值为k的用户的个数。 
-// 数据范围n <= 300000,q<=300000 k是整型
