8eed30594e55d429e71e5032eafa8634216e85c7
none Frontier <liuyiquancov5@gmail.com>
2019-04-13 13:01:02 +0800

    add 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
+import { createInterface } from 'readline';
+let rl = createInterface({
 	input:process.stdin,
 	output:process.stdout
 }); 
@@ -12,4 +12,49 @@ let count = 0,
 	err = 0;
 
 
-let Beads = [5,6,7];
+let Beads = {};
+let last;
+
+rl.on('line',(line)=>{
+	if(count==0){
+		[n,m,c] = line.split(' ').map(ele=>+ele);
+		count++;
+		last = n - m;
+	}
+
+	else if(count <= n ){
+		let nowBead = line.split(' ');
+		let ColorNum = +nowBead[0];
+		
+		for(let i=1;i<=ColorNum;i++){
+			let nowColor = nowBead[i];
+			if(Beads[nowColor]=='err'){
+				continue;
+			}else if(!Beads[nowColor]){
+				Beads[nowColor] = [count, count];
+			}else{
+				let BNC = Beads[nowColor];
+				if(count - BNC[1] > m-1){
+					BNC[1]=count;
+					if(count - last){
+						if(BNC[0]+n-count <= m-1){
+							Beads[nowColor] = 'err';
+							err++;
+						}
+					}
+
+				}else{
+					Beads[nowColor] = 'err';
+					err++;
+				}
+			
+			}
+		}
+
+		count++;
+		if(count>n){
+			console.log(err);
+			rl.close();
+		}
+	}
+})
